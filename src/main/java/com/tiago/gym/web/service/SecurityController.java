584a93b0de08d225dbc65a2c4aa677089106e067
Tiago Melo <tiagotele@gmail.com>
2016-12-27 17:30:52 -0300

    Adding custom login form based on Mkyong tutorial.

--- src/main/java/com/tiago/gym/web/service/SecurityController.java
+++ src/main/java/com/tiago/gym/web/service/SecurityController.java
@@ -3,11 +3,12 @@ package com.tiago.gym.web.service;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SecurityController {
-	
+
 	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
 	public ModelAndView welcomePage() {
 
@@ -26,6 +27,25 @@ public class SecurityController {
 		model.addObject("title", "Spring Security Hello World");
 		model.addObject("message", "This is protected page!");
 		model.setViewName("admin");
+
+		return model;
+
+	}
+
+	// Spring Security see this :
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
 
 		return model;
 
