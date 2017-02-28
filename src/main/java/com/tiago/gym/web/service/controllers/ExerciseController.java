b15855b82ffcc5b278f6684152ae1aa5aa510010
Tiago Melo <tiagotele@gmail.com>
2017-02-27 23:44:23 -0300

    Changing exercise structure.

--- src/main/java/com/tiago/gym/web/service/controllers/ExerciseController.java
+++ src/main/java/com/tiago/gym/web/service/controllers/ExerciseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tiago.gym.web.service.models.Equipment;
 import com.tiago.gym.web.service.models.Exercise;
 
 @Controller
@@ -14,9 +15,16 @@ public class ExerciseController {
 	@RequestMapping(value = "/exercise", method = RequestMethod.GET)
 	public @ResponseBody Exercise get(
 			@RequestParam(value = "id", required = true, defaultValue = "1") Long id) {
+		
+		Equipment equipment = new Equipment();
+		equipment.setMachineNumber(10);
+		equipment.setName("supino");
+		
 		Exercise exercise = new Exercise();
 		exercise.setName("on get");
 		exercise.setTarget("target");
+		exercise.setEquipment(equipment);
+		
 		return exercise;
 	}
 	
--- src/main/java/com/tiago/gym/web/service/models/Exercise.java
+++ src/main/java/com/tiago/gym/web/service/models/Exercise.java
@@ -6,6 +6,7 @@ public class Exercise implements Serializable{
 	private static final long serialVersionUID = -7788619177798333712L;
 	private String name;
 	private String target;
+	private Equipment equipment;
 	
 	public Exercise(){
 		
@@ -23,4 +24,14 @@ public class Exercise implements Serializable{
 	public void setTarget(String target) {
 		this.target = target;
 	}
+
+	public Equipment getEquipment() {
+		return equipment;
+	}
+
+	public void setEquipment(Equipment equipment) {
+		this.equipment = equipment;
+	}
+	
+	
 }
